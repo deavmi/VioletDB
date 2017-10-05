43ec3877e27c7580c797b8012b69f2351603c471
Tristan B. Kildaire <deavmi@disroot.org>
2017-10-05 19:37:30 +0200

    Add files via upload

--- server.d
+++ server.d
@@ -5,7 +5,47 @@ import core.Thread;
 import database;
 import std.stdio;
 
+Connection[] connections;
+
+class Connection : Thread
+{
+
+	Socket client;
+
+	this(Socket client)
+	{
+		//this.client = client;
+		super(&doWork);
+		this.client = client;
+	}
+
+	void doWork()
+	{
+		while(true)
+		{
+			writeln("Working working...");
+		}
+	}
+
+}
+
 void startServer(Database db, ushort port)
 {
 	writeln("Starting server on port ", port, " with database '", db.filename , "'...");
+
+	AddressInfo addrInfo = AddressInfo();
+	addrInfo.family = AddressFamily.INET;
+	addrInfo.protocol = ProtocolType.TCP; //try out RAW
+	addrInfo.type = SocketType.STREAM;
+	Socket servSock = new Socket(addrInfo);
+	servSock.bind(parseAddress("0.0.0.0", port));
+	servSock.listen(1); //what is the backlog? How many connectiosn can be qeued?
+	while (true)
+	{
+		Socket clientSock = servSock.accept();
+		Connection conn = new Connection(clientSock);
+		connections[connections.length++] = conn;
+		conn.start();
+	}
+
 }
