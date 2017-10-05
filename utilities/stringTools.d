b3d9d8078eb386090e4704f55a10c51921d45e82
Tristan B. Kildaire <deavmi@disroot.org>
2017-10-05 20:24:43 +0200

    Add files via upload

---
+++ utilities/stringTools.d
@@ -0,0 +1,12 @@
+module utilities.stringTools;
+
+//fuck that module name is annoying
+
+string chomp(string str) //if only I knew where this was in std package
+{
+	ubyte[] strBs = cast(ubyte[])str;
+	strBs.length--;
+	return cast(string)strBs;	
+}
+
+
