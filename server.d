b171cbe9a22d0a6171658a7b8e70c7d8cf12eece
Tristan B. Kildaire <deavmi@disroot.org>
2017-10-05 19:04:18 +0200

    Updates

--- server.d
+++ server.d
@@ -3,8 +3,9 @@ module server;
 import std.socket;
 import core.Thread;
 import database;
+import std.stdio;
 
 void startServer(Database db, ushort port)
 {
-	
+	writeln("Starting server on port ", port, " with database '", db.filename , "'...");
 }
