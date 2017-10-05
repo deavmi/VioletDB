fe66c23aaa23ba92824564ec80e27e750bfcf685
Tristan B. Kildaire <deavmi@disroot.org>
2017-10-05 19:00:05 +0200

    Updates (whatever)

---
+++ server.d
@@ -0,0 +1,10 @@
+module server;
+
+import std.socket;
+import core.Thread;
+import database;
+
+void startServer(Database db, ushort port)
+{
+	
+}
--- violet.d
+++ violet.d
@@ -1,16 +1,33 @@
 import std.stdio;
 import database;
-
 import cruft_dont_touch_just_leave_it_alone_to_do;
+import server;
+import std.File;
 
 void main()
 {
 
 	write("Before we can start, enter the database file to use:");
 	string filename = chomp(readln());
-	writeln(filename.length);
-	//testing code
-	Database db = new Database(filename);
-	db.addKey("nasme","string");
+	//ushort port = getPort(chomp(readln())); //oh hell I don't know how to use std.conv (we're in for a ride) (grabs ascii table, or not)
+	//too hard for now. Will do later
+	//writeln(filename.length);
+	if (exists(filename))
+	{
+		ushort port = 25515;
+		//testing code
+		Database db = new Database(filename);
+		db.addKey("nasme","string");
+		startServer(db, port);
+	}
+	else
+	{
+		writeln("File '", filename, "' not found.");
+	}
+
+
+	//How does one concatenate in Dlang? I'm going to havr to write something for that (not that I need it)
+
+	
 
 }
