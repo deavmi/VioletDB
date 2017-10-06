a1f28968085f7dfcad4c1fd777dd4d6a4fde1152
Tristan B. Kildaire <deavmi@disroot.org>
2017-10-06 17:25:17 +0200

    Add files via upload

--- misc/out2.d
+++ misc/out2.d
@@ -1,6 +1,9 @@
 module misc.out2;
 
+import std.datetime.date : DateTime;
+
 string prettyStr(string service, string message)
 {
-	return "["~service~"] "~message;
+	string time = DateTime().toString();
+	return "[" ~ service ~ "] (" ~ time ~ ") " ~ message;
 }
